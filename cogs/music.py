2990d05005965b3c78c8e625551aca193e3f9825
Sand <168356842+Sandau1204@users.noreply.github.com>
2025-11-28 17:52:54 +0700

    fix

--- cogs/music.py
+++ cogs/music.py
@@ -45,11 +45,11 @@ YDL_OPTIONS = {
     #    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     #},
     'cachedir': False,
-    'extractor_args': {
-        'youtube': {
-            'player_client': ['android', 'web']
-        }
-    }
+    #'extractor_args': {
+    #    'youtube': {
+    #        'player_client': ['android', 'web']
+    #    }
+    #}
 }
 
 FFMPEG_OPTIONS = {
