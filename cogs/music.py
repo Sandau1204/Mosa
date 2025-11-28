7998469286f7131ab21b8ccb275cc03d12a6df10
Sand <168356842+Sandau1204@users.noreply.github.com>
2025-11-28 17:40:02 +0700

    fix ydl

--- cogs/music.py
+++ cogs/music.py
@@ -41,9 +41,9 @@ YDL_OPTIONS = {
     'nocheckcertificate': True,
     'cookiefile': 'cookies.txt',
     # --- THÊM PHẦN NÀY ---
-    'http_headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    },
+    #'http_headers': {
+    #    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    #},
     'cachedir': False,
 }
 
